7f0d1021e018cbf09952048839e3e10a66bb0c59
Nelson Moses <nmoses@tracom.co.ke>
2019-03-25 13:20:28 +0300

    Initial commit

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,37 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Mr. Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' },
+      { id: 21, name: 'lacazete' },
+      { id: 22, name: 'abumayang' },
+      { id: 23, name: 'lukaku' },
+
+      { id: 24, name: 'harry' },
+      { id: 25, name: 'thiery' },
+      { id: 26, name: 'Nelson' },
+      { id: 27, name: 'Moses' }
+    ];
+    return {heroes};
+  }
+
+
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,22 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+  { id: 21, name: 'lacazete' },
+  { id: 22, name: 'abumayang' },
+  { id: 23, name: 'lukaku' },
+
+  { id: 24, name: 'harry' },
+  { id: 25, name: 'thiery' },
+  { id: 26, name: 'Nelson' },
+  { id: 27, name: 'Moses' }
+];
